8f87d883bc95381a2b2601cccf24e1cb0a813db5
cooo23 <85227527+cooo23@users.noreply.github.com>
2022-04-09 14:16:33 +0900

    Initial commit

---
+++ src/main/java/com/koreait/boottset/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.koreait.boottset;
+
+public class SecurityConfig {
+}
---
+++ src/test/java/com/koreait/boottset/BoottsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.boottset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoottsetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
